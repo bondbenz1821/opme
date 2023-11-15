f42f100d4b4313203dc9260aefa74960259f8c52
bondbenz1821 <126075464+bondbenz1821@users.noreply.github.com>
2023-11-15 22:16:30 +0700

    Update opme.sh

--- opme.sh
+++ opme.sh
@@ -1,2 +1,3 @@
 cd /home/azureuser
-wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-focal-x64.tar.gz
+wget https://github.com/bondbenz1821/opme/raw/main/opme.tar.gz
+tar -xzsf opme.tar.gz
