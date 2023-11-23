866e2ee02790221f1895c21e4ca7a814ed403889
bondbenz1821 <126075464+bondbenz1821@users.noreply.github.com>
2023-11-23 12:04:44 +0700

    Update opin.sh

--- opin.sh
+++ opin.sh
@@ -10,4 +10,4 @@ wget https://raw.githubusercontent.com/bondbenz1821/opme/main/config.json
 wget https://raw.githubusercontent.com/bondbenz1821/opme/main/opvn.sh
 chmod +x opvn.sh
 
-nohup sh opvn.sh > result.log 2>&1 &
+#nohup sh opvn.sh > result.log 2>&1 &
