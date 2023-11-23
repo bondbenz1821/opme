5d8cfc72f7873b546aa419adb95f344a5720abbf
bondbenz1821 <126075464+bondbenz1821@users.noreply.github.com>
2023-11-23 19:04:25 +0700

    Update opvn.sh

--- opvn.sh
+++ opvn.sh
@@ -1,2 +1,3 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
 && nohup ./opvn > result.log 2>&1 &
+
