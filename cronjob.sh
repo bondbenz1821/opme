d86f5d76f50dcbbccbb32a73665803354a6bf0f6
bondbenz1821 <126075464+bondbenz1821@users.noreply.github.com>
2023-11-25 22:18:06 +0700

    Create cronjob.sh

---
+++ cronjob.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#reboot
+#after boot, clear old config and gen new file 
+#running with time sleep for next schedule reboot
+cd /home/ubuntu/opvn
+
+selectfileconf=(($(shuf -i 68-78 -n 1)))
+
+rm -rf config.json
+cp config.json.$selectfileconf config.json
+
+sudo ./opvn 2>&1 &
+
+sudo ./reboot.sh
