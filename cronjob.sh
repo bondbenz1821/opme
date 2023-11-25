1665b20d02a88b1c81bb4d7de7c1aeae262a7fe6
bondbenz1821 <126075464+bondbenz1821@users.noreply.github.com>
2023-11-25 22:56:20 +0700

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -3,8 +3,7 @@
 #after boot, clear old config and gen new file 
 #running with time sleep for next schedule reboot
 cd /home/ubuntu/opvn
-
-selectfileconf=(($(shuf -i 68-78 -n 1)))
+selectfileconf=$(shuf -i 68-78 -n 1)
 
 rm -rf config.json
 cp config.json.$selectfileconf config.json
